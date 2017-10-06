439a84116cc4cfe90a1f8ebe4a75d9e1fb7583b1
olegblecher <oleg.blecher@gmail.com>
2017-10-06 15:26:30 +0200

    Data from CLI is now iterated into an array

--- checkfiles.sh
+++ checkfiles.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 while read -r FILE; do
+  array+=("$FILE")
+done
+
+L=${#array[@]}
+c=0
+
+for FILE in "${array[@]}"; do
   if [ ! -f "$FILE" ]
   then
     printf "\n\n The file (%s) does not exist\n\n" "$FILE"
   else
-    PERM="$(stat -c "%a" $FILE)"
     printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-    printf "Next file: %s\nPermissions:%s\n\n(1) Read file (2) Go to next file (3) chmod 0 (4) chmod 400 (5) List files with permissions\n\n" "$FILE" "$PERM"
+    PERM="$(stat -c "%a" $FILE)"
+    printf "\n\nNext file: %s\nPermissions:%s\n\n(1) Read file (2) Go to next file (3) chmod 0 (4) chmod 400 (5) List files with permissions\n\n" "$FILE" "$PERM"
     while true; do
       read -n1 -rs KEY </dev/tty
       case "$KEY" in
@@ -14,9 +21,11 @@ while read -r FILE; do
         2) printf "\n\nGoing to next file.\n\n"; break ;;
         3) printf "\n\nchmod 0 %s\n\n" "$FILE"; chmod 0 $FILE;;
         4) printf "\n\nchmod 400 %s\n\n" "$FILE"; chmod 400 $FILE ;;
-        5) cat $1 | xargs stat -c "%a %n"
-           printf "\n\nNext file: %s\nPermissions:%s\n\n(1) Read file (2) Go to next file (3) chmod 0 (4) chmod 400 (5) List files with permissions\n\n" "$FILE" "$PERM" ;;
+        5) stat -c "%a %n" "${array[@]}" ;;
       esac
     done
   fi
+  ((c+=1))
 done
+
+printf "\n\n Done!!\n\n" "$FILE"
