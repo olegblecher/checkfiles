33abd06ca29a5091883b16c0af824c698b2f04f7
olegblecher <oleg.blecher@gmail.com>
2017-10-20 16:01:02 +0200

    Update checkfiles.sh
    Can now go back a file.

--- checkfiles.sh
+++ checkfiles.sh
@@ -3,14 +3,15 @@ while read -r FILE; do
   array+=("$FILE")
 done
 
-for FILE in "${array[@]}"; do
+for ((n=0;n<${#array[@]};n++)); do
+  FILE="${array[n]}"
   if [ ! -f "$FILE" ]
   then
     printf "\n\n The file (%s) does not exist\n\n" "$FILE"
   else
     printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
     PERM="$(stat -c "%a" $FILE)"
-    printf "\n\nNext file: %s\nPermissions:%s\n\n(1) Read file (2) Go to next file (3) chmod 0 (4) chmod 400 (5) List files with permissions\n\n" "$FILE" "$PERM"
+    printf "\n\nNext file: %s\nPermissions:%s\n\n(1) Read file (2) Go to next file (3) chmod 0 (4) chmod 400 (5) List files with permissions (6) Previous file\n\n" "$FILE" "$PERM"
     while true; do
       read -n1 -rs KEY </dev/tty
       case "$KEY" in
@@ -19,6 +20,12 @@ for FILE in "${array[@]}"; do
         3) printf "\n\nchmod 0 %s\n\n" "$FILE"; chmod 0 $FILE;;
         4) printf "\n\nchmod 400 %s\n\n" "$FILE"; chmod 400 $FILE ;;
         5) stat -c "%a %n" "${array[@]}" ;;
+        6) if [[ "$n" < 1 ]]
+           then
+             printf "\n\nNo previous file to go to!\n\n" ;
+           else
+             printf "\n\nGoing to previous file.\n\n"; ((n-=2)); break ;
+           fi
       esac
     done
   fi
